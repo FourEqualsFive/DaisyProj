36ed5da9bae73e3a92a1b8eb6582f8d9ba91cc58
FourEqualsFive <cody_robson@hotmail.ca>
2025-03-11 13:37:09 -0400

    fixed limiter, increased block size, removed nOsc averaging from callback

--- seed/Drum_Machine/Drum_Machine.cpp
+++ seed/Drum_Machine/Drum_Machine.cpp
@@ -4,9 +4,8 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                    AudioHandle::InterleavingOutputBuffer out,
                    size_t                                size)
 {
-    int nOsc = 0;
-    int deadClick = 0, deadBloop = 0, deadTiss = 0;
-    int deadSnare = 0, deadBass = 0;
+    unsigned int deadClick = 0, deadBloop = 0, deadTiss = 0;
+    unsigned int deadSnare = 0, deadBass = 0;
     float freq_set, freqMod;
     float bassOsc_out, snareOsc_out = 0;
     float clickOsc_out, bloopOsc_out;
@@ -28,9 +27,10 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     //Prepare the audio block
     for(size_t i = 0; i < size; i += 2)
     {
-        /*******************************************************************
+        /*******************************************************************************
+         * \todo Change voice gate to work on env length instead of signal amplitude
          * \todo Process in larger blocks, test latency tradeoff
-        *******************************************************************/
+        *******************************************************************************/
 
         freqMod = (1 + hardware.adc.GetFloat(0));
         freq_set = base_f * freqMod;
@@ -38,57 +38,50 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
         //Process the next samples
         //Increment oscillator count for active processes
         //Use individual gate and deadcounts to skip processing when no output
+
         if (snrGate) {
             snareOsc_out = snareEnv.Process();
-            (snareOsc_out > -0.1) && (snareOsc_out < 0.1) ? 0 : nOsc++ ;
             (snareOsc_out > -0.1) && (snareOsc_out < 0.1) ? deadSnare++ : deadSnare = 0;
-            (deadSnare > 4) ? snrGate = false : snrGate = true;
+            (deadSnare > size) ? snrGate = false : snrGate = true;
         }
         else { snareOsc_out = 0; }
 
         if (bssGate) {
             bassOsc_out = 5 * bassEnv.Process();
-            (bassOsc_out > -0.1) && (bassOsc_out < 0.1) ? 0 : nOsc++ ;
             (bassOsc_out > -0.1) && (bassOsc_out < 0.1) ? deadBass++ : deadBass = 0;
-            (deadBass > 4) ? bssGate = false : bssGate = true;
+            (deadBass > size) ? bssGate = false : bssGate = true;
         }
         else { bassOsc_out = 0; }
 
         if (blpGate) {
             bloopOsc_out = bloopOsc.CallBack(freq_set, bloopFreqGate, bloopVolGate);
-            (bloopOsc_out > -0.1) && (bloopOsc_out < 0.1) ? 0 : nOsc++ ;
             (bloopOsc_out > -0.1) && (bloopOsc_out < 0.1) ? deadBloop++ : deadBloop = 0;
-            (deadBloop > 4) ? blpGate = false : blpGate = true;
+            (deadBloop > size) ? blpGate = false : blpGate = true;
         }
         else { bloopOsc_out = 0; }
 
         if (clkGate) {
             clickOsc_out = .5 * clickOsc.CallBack(freq_set);
-            (clickOsc_out > -0.1) && (clickOsc_out < 0.1) ? 0 : nOsc++ ;
             (clickOsc_out > -0.1) && (clickOsc_out < 0.1) ? deadClick++ : deadClick = 0;
-            (deadClick > 4) ? clkGate = false : clkGate = true;
+            (deadClick > size) ? clkGate = false : clkGate = true;
         }
         else { clickOsc_out = 0; }
 
 
         if (tssGate) {
             noise_out = tissNoise.Callback();
-            (noise_out > -0.1) && (noise_out < 0.1) ? 0 : nOsc++ ;
             (noise_out > -0.1) && (noise_out < 0.1) ? deadTiss++ : deadTiss = 0;
-            (deadTiss > 4) ? tssGate = false : tssGate = true;
+            (deadTiss > size) ? tssGate = false : tssGate = true;
         }
         else { noise_out = 0; }
-        /*********************************************************
-         *\todo add proper compression instead of this nonsense
-        *********************************************************/
+        
+        //Sum all voices, save the result for block processing
         sig[i] = noise_out + clickOsc_out + bassOsc_out + snareOsc_out + bloopOsc_out;
-        sig[i] = sig[i] / (nOsc + 1);
-        nOsc = 0;
 
     } // end audio processing
 
     //Soft limit the oscillator samples to prevent clipping
-    //limit.ProcessBlock(sig,1,1);
+    limit.ProcessBlock(sig,size,1);
 
     //Send to output
     for(size_t i = 0; i < size; i += 2) {
@@ -103,8 +96,9 @@ int main(void)
     // Configure and Initialize the Daisy Seed
     hardware.Configure();
     hardware.Init();
-    hardware.SetAudioBlockSize(4);
+    hardware.SetAudioBlockSize(128);
     float samplerate = hardware.AudioSampleRate();
+    int blocksize = hardware.AudioBlockSize();
     
     //Initialize adc & start reading values
     adcConfig.InitSingle(hardware.GetPin(15));
@@ -123,11 +117,11 @@ int main(void)
     
     //Initialize buttons on pins 24 - 28
     //The callback rate is samplerate / blocksize (48)
-    bloop.Init(hardware.GetPin(24), samplerate / 48.f);
-    snare.Init(hardware.GetPin(25), samplerate / 48.f);
-    bass.Init(hardware.GetPin(26), samplerate / 48.f);
-    tiss.Init(hardware.GetPin(27), samplerate / 48.f);
-    click.Init(hardware.GetPin(28), samplerate / 48.f);
+    bloop.Init(hardware.GetPin(24), samplerate / blocksize);
+    snare.Init(hardware.GetPin(25), samplerate / blocksize);
+    bass.Init(hardware.GetPin(26), samplerate / blocksize);
+    tiss.Init(hardware.GetPin(27), samplerate / blocksize);
+    click.Init(hardware.GetPin(28), samplerate / blocksize);
 
     //Start calling the callback function
     hardware.StartAudio(AudioCallback);
